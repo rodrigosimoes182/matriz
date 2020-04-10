9fc7eb9afbb62dee54545ff491f80f2dd8d10df5
rodrigosimoes182 <rodrigo.simoes182@gmail.com>
2020-04-10 17:44:31 -0300

    git remote add origin https://github.com/rodrigosimoes182/matriz.git

---
+++ bokeh01.py
@@ -0,0 +1,27 @@
+from bokeh.plotting import figure, output_file, show
+import numpy as np
+
+#create a matriz
+a = np.arange(64).reshape(2,32)
+
+# prepare some data
+x = a[0]
+
+y = a[1]
+print(x, y)
+
+# output to static HTML file
+output_file("/Users/rodsim/Documents/matrizes/templates/lines.html")
+
+# create a new plot with a title and axis labels
+p = figure(
+   tools="pan,box_zoom,reset,save",
+   y_axis_type="log", y_range=[0.001, 10**11], title="log axis example",
+   x_axis_label='sections', y_axis_label='particles'
+)
+
+# add a line renderer with legend and line thickness
+p.line(x, y, legend_label="y=10^x", line_color="red")
+
+# show the results
+show(p)
